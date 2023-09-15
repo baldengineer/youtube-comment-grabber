bcac218af9ca192ae28b155e825fa7bb2ee7669e
baldengineer <james@baldengineer.com>
2023-09-15 17:55:58 -0500

    Updated readme for venv

--- youtube_comment_grabber.py
+++ youtube_comment_grabber.py
@@ -11,7 +11,6 @@ from stuff import handle_video_ids
 
 # TODO: Incorporate Rich (and maybe Textual) https://github.com/Textualize/rich
 
-print_help = True # just need a dummy line to try something in git
 print_help = False
 
 if ("-h" in sys.argv) or ("-H" in sys.argv):
